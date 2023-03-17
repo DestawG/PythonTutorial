5f5b1da1256e75a16e8061e6871f795c1545de6c
Destaw <destawg@gmail.com>
2023-03-17 13:50:06 +0300

    conditionalllop

--- conditionalloop.py
+++ conditionalloop.py
@@ -2,14 +2,33 @@
 # while loop if something not greaterthan it print 
 # i=1
 # while i<6:
-#     print(i)
-#     if i==3:
-#         break
-#     i+=1
+#   print(i)
+#   if i==3:
+#     break
+# i+=1
     
-i=0
-while i<6:
-    print(i)
-    i+=1
-else:
-    print(" it is 6")
+# i=0
+# while i<6:
+#      print(i)
+#      i+=1
+# else:
+#      print(" it is 6")
+
+# # for loop
+# student= ["Desatw","Gisme","Mulachew"]
+# for stud in student :
+#     print(stud)
+
+# name="Destaw"
+# for x in name:
+#     print(x)
+# for x in range(10):
+#         print(x)
+# for x in range(2,16,2):
+#     print(x)
+# loop under loop
+adi=["red","big","tasty"]
+fruits=["apple","banana","cherry"]
+for x in adi:
+    for y in fruits:
+        print(x,y)
