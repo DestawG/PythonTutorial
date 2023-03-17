8a096861c3f58c31aa2f48d4b8013141684c48c5
Destaw <destawg@gmail.com>
2023-03-17 11:54:01 +0300

    Operator

--- oper.py
+++ oper.py
@@ -1,7 +1,23 @@
 # arithmetic Operator
-nume1=12 
-num2=14
-sum=num2 + nume1
-print(sum)
-# sum, num1, num2 are operands
+# nume1=12 
+# num2=14
+# sum=num2 + nume1
+# print(sum)
+# # sum, num1, num2 are operands
+# #  = and + are operator 
+# nume1+=2 # num1 = num1+2
+# num2-2# num2=num2-1
+# print(num2, nume1)
+# nume1**2
+# print(nume1)
+# # = assignment operatort 
+# print(nume1==num2)
+# logical operator(and , or )
+num1=12
+print(num1>10 and num1<10)
+print(num1>10 and num1<15)
+print(num1>10 or num1<9)
+print(num1<10 or num1<8)
+print(num1!=13)
+print(num1!=12)
 
