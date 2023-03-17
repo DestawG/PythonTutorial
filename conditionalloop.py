5e56b98cd038dbe61f20c70b95b2a9be79baba51
Destaw <destawg@gmail.com>
2023-03-17 12:24:17 +0300

    loop

---
+++ conditionalloop.py
@@ -0,0 +1,15 @@
+# loop with while and for 
+# while loop if something not greaterthan it print 
+# i=1
+# while i<6:
+#     print(i)
+#     if i==3:
+#         break
+#     i+=1
+    
+i=0
+while i<6:
+    print(i)
+    i+=1
+else:
+    print(" it is 6")
