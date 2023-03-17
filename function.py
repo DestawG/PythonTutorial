0680714464b2a2d5fb93e7d839b06d7bc5daa2b8
Destaw <destawg@gmail.com>
2023-03-17 14:03:12 +0300

    Function

---
+++ function.py
@@ -0,0 +1,20 @@
+# function has two type
+# built -in function
+# user-defined function 
+# def functionname(parameters): 
+# num1=12
+# num2=23
+# sum=num1 + num2 
+# product=num1*num2
+# que= num1/num2
+# print(sum,product,que)
+
+def sum(n1,n2):
+    return n1+n2
+def product(n1,n2):
+    return n1*n2
+def que(n1,n2):
+    return n1/n2
+print(sum(12,12))
+print(product(5,5))
+print(que(25,5))
